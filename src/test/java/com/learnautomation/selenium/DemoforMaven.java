831887a59c6524383d2e5a9e2a81a43091cbfb16
krish3402 <mk.tala654@gmail.com>
2019-09-25 16:08:11 +0530

    hello

---
+++ src/test/java/com/learnautomation/selenium/DemoforMaven.java
@@ -0,0 +1,13 @@
+package com.learnautomation.selenium;
+
+import org.testng.annotations.Test;
+
+public class DemoforMaven {
+	
+	@Test
+	public void testHello() {
+		
+		System.out.println("Welcome to Maven World");
+	}
+
+}
